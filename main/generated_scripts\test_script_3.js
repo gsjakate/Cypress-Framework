c31f991721ba9b37ebd8b9af0a76eda51ccb5b46
gsjakate <govind-sureshrao.jakate@capgemini.com>
2025-11-25 09:13:45 +0530

    Update test_script_3.js

--- main/generated_scripts\test_script_3.js
+++ main/generated_scripts\test_script_3.js
@@ -1,2 +1,2 @@
 // Cypress script for: TC_02
-// Fallback: Could not generate script. Error: 'Agent' object has no attribute 'run'
+// Fallback: Could not generate script. Error: 'Agent' object has no attribute 'execute'
